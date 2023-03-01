b0742fa50b485c51c5ef541a37f7931731af4944
Marko Petkovic <marko.petkovic@vaulted-oak.com>
2023-03-01 10:43:57 +0100

    product image

---
+++ src/scripts/entries/section/product-image.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import ProductImage from 'modules/product-image/product-image.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(ProductImage, 'ProductImage')
+})

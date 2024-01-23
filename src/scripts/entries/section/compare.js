fe515f0927fc5ac4766d6c734747420326eb314d
Marko Petkovic <markopetkovic@Markos-MacBook-Pro.local>
2024-01-23 12:55:44 +0100

    compare table

---
+++ src/scripts/entries/section/compare.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import Compare from 'modules/compare/compare.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(Compare, 'Compare')
+})

1b837584cc70065b1e0003d49b35c21e85f0fd32
Dusan639 <dusan16stojanovic@gmail.com>
2024-02-06 15:50:59 +0100

    media banner section - lola

---
+++ src/scripts/entries/section/media-banner.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import MediaBanner from 'modules/media-banner/section-media-banner.js' 
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(MediaBanner, 'MediaBanner')
+})

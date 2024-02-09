33f5349d07a5f4072cb3f5f95892cfa1ea612004
Marko Petkovic Drugi <markopetkovic@MacBook-Pro-2.local>
2024-02-09 13:42:12 +0100

    mako fixes

--- src/modules/pdp-hero/section-pdp-hero.js
+++ src/modules/pdp-hero/section-pdp-hero.js
@@ -1,5 +1,40 @@
 import './section-pdp-hero.css'
 
+const atcButton = document.querySelector('.section-pdp-hero .js-atc');
+const checkBox = document.getElementById('pdp-hero__checkbox');
+
+atcButton.addEventListener('click', () => {
+    const quantity = checkBox.checked ? 2 : 1;
+    let addItems = [];
+    atcButton.dataset.quantity = quantity;
+    addItems = [
+        {
+          id: atcButton.getAttribute('data-product'),
+          quantity: quantity
+        }
+    ]
+    const formData = {
+        items: addItems
+    }
+    fetch(window.Shopify.routes.root + 'cart/add.js', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+        .then(response => {
+          console.log(response.status, 'ok')
+          if (response.status === 200) {
+            console.log(addToCart, 'ok')
+          }
+          return response.json()
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+        })
+});
+
 var pdpHeroSwiper = document.querySelector('.pdp-hero__gallery-main');
 var pdpHeroThumbsSwiper = document.querySelector('.pdp-hero__gallery-thumbs');
 var pdpHeroThumbs = pdpHeroThumbsSwiper?.querySelectorAll('.pdp-hero__gallery-thumbs .swiper-slide');
@@ -33,6 +68,7 @@ if (descriptionWrapper) {
     const productDescription = document.querySelector('.section-pdp-hero .pdp-hero__product-description-wrapper');
     const productDescriptionCollapse = document.querySelector('.pdp-hero__description-collapse');
 
+<<<<<<< Updated upstream
     productDescriptionCollapse.addEventListener('click', () => {
         descriptionWrapper.classList.toggle('collapse-text');
 
@@ -50,3 +86,13 @@ checkBox.addEventListener('click', () => {
     const quantity = checkBox.checked ? 2 : 1;
     atcButton.dataset.quantity = quantity;
 });
+=======
+productDescriptionCollapse.addEventListener('click', () => {
+    descriptionWrapper.classList.toggle('collapse-text');
+
+    productDescription.style.maxHeight = productDescription.scrollHeight + 'px';
+    !descriptionWrapper.classList.contains('collapse-text')
+        ? productDescription.style.maxHeight = null
+        : productDescription.style.maxHeight = productDescription.scrollHeight + 'px';
+})
+>>>>>>> Stashed changes

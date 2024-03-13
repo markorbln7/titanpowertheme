a206ca6d209180d51810c70141a63b6f82924245
Dusan639 <dusan16stojanovic@gmail.com>
2024-03-14 00:50:19 +0100

    pdp-hero v4

---
+++ src/scripts/entries/section/pdp-hero-v4.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import PdpHeroV4 from 'modules/pdp-hero-v4/section-pdp-hero-v4.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(PdpHeroV4, 'PdpHeroV4')
+})

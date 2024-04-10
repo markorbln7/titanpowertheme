b3b0e4a2132880bb17e0da017f7fff540cb05d49
Dusan639 <dusan16stojanovic@gmail.com>
2024-04-10 17:23:41 +0200

    product zigzag

---
+++ src/modules/product-zigzag/product-zigzag.js
@@ -0,0 +1,1 @@
+import './product-zigzag.css'
---
+++ src/scripts/entries/section/product-zigzag.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import ProductZigzag from 'modules/product-zigzag/product-zigzag.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(ProductZigzag, 'ProductZigzag')
+})

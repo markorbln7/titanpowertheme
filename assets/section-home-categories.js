540388015a0b1d68741f630a33b5b2bff4916e9d
Marko Petkovic Drugi <markopetkovic@MacBook-Pro-2.local>
2024-06-18 11:39:17 +0200

    cat filter

---
+++ assets/section-home-categories.js
@@ -0,0 +1,50 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkshopify_base_2_0"] = self["webpackChunkshopify_base_2_0"] || []).push([["section-home-categories"],{
+
+/***/ "./src/modules/home-categories/home-categories.js":
+/*!********************************************************!*\
+  !*** ./src/modules/home-categories/home-categories.js ***!
+  \********************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _home_categories_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home-categories.css */ "./src/modules/home-categories/home-categories.css");
+
+
+/***/ }),
+
+/***/ "./src/scripts/entries/section/home-categories.js":
+/*!********************************************************!*\
+  !*** ./src/scripts/entries/section/home-categories.js ***!
+  \********************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lib_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lib/components */ \"./src/scripts/lib/components.js\");\n/* harmony import */ var modules_home_categories_home_categories_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! modules/home-categories/home-categories.js */ \"./src/modules/home-categories/home-categories.js\");\n\n\ndocument.addEventListener('DOMContentLoaded', () => {\n  (0,lib_components__WEBPACK_IMPORTED_MODULE_0__.initComponent)(modules_home_categories_home_categories_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"], 'HpCat');\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyaXB0cy9lbnRyaWVzL3NlY3Rpb24vaG9tZS1jYXRlZ29yaWVzLmpzIiwibWFwcGluZ3MiOiI7OztBQUFBO0FBQ0E7QUFFQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaG9waWZ5LWJhc2UtMi4wLy4vc3JjL3NjcmlwdHMvZW50cmllcy9zZWN0aW9uL2hvbWUtY2F0ZWdvcmllcy5qcz9jNDVjIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGluaXRDb21wb25lbnQgfSBmcm9tICdsaWIvY29tcG9uZW50cydcbmltcG9ydCBIcENhdCBmcm9tICdtb2R1bGVzL2hvbWUtY2F0ZWdvcmllcy9ob21lLWNhdGVnb3JpZXMuanMnXG5cbmRvY3VtZW50LmFkZEV2ZW50TGlzdGVuZXIoJ0RPTUNvbnRlbnRMb2FkZWQnLCAoKSA9PiB7XG4gIGluaXRDb21wb25lbnQoSHBDYXQsICdIcENhdCcpXG59KSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/scripts/entries/section/home-categories.js\n");
+
+/***/ }),
+
+/***/ "./src/modules/home-categories/home-categories.css":
+/*!*********************************************************!*\
+  !*** ./src/modules/home-categories/home-categories.css ***!
+  \*********************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbW9kdWxlcy9ob21lLWNhdGVnb3JpZXMvaG9tZS1jYXRlZ29yaWVzLmNzcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaG9waWZ5LWJhc2UtMi4wLy4vc3JjL21vZHVsZXMvaG9tZS1jYXRlZ29yaWVzL2hvbWUtY2F0ZWdvcmllcy5jc3M/OWIzYSJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/modules/home-categories/home-categories.css\n");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["commons"], function() { return __webpack_exec__("./src/scripts/entries/section/home-categories.js"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
---
+++ src/scripts/entries/section/home-categories.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import HpCat from 'modules/home-categories/home-categories.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(HpCat, 'HpCat')
+})

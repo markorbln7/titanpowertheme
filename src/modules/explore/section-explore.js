60c97876a107cc84212318bb8e6858ff85ab07f5
Dusan639 <dusan16stojanovic@gmail.com>
2024-10-11 17:43:33 +0200

    buy now popup

--- src/modules/explore/section-explore.js
+++ src/modules/explore/section-explore.js
@@ -1,27 +1,27 @@
 import './section-explore.css'
 
 document.addEventListener('DOMContentLoaded', function () {
+    //Product cards hover logic
     const productCards = document.querySelectorAll('.section-explore__product');
-    const productContents = document.querySelectorAll('.section-explore__product-content > div');
     const desktopLabel = document.querySelector('.section-explore__collection-label-desktop');
     const travelLabel = document.querySelector('.section-explore__collection-label-travel');
 
-    let activeProduct = null; 
-    let activeCard = null; 
+    let activeProduct = null;
+    let activeCard = null;
 
     const initialProduct = document.querySelector('.section-explore__product-content [data-product="for-desktop-1"]');
     if (initialProduct) {
         initialProduct.style.opacity = '1';
         initialProduct.style.transition = 'opacity 0.5s ease';
-        desktopLabel.classList.add('active'); 
-        const initialLines = initialProduct.querySelectorAll('.animate-line'); 
+        desktopLabel.classList.add('active');
+        const initialLines = initialProduct.querySelectorAll('.animate-line');
         initialLines.forEach(line => line.classList.add('active-line'));
-        activeProduct = initialProduct; 
+        activeProduct = initialProduct;
 
         const firstCard = document.querySelector('.section-explore__product[data-product-hover="for-desktop-1"]');
         if (firstCard) {
             firstCard.classList.add('active');
-            activeCard = firstCard; 
+            activeCard = firstCard;
         }
     }
 
@@ -49,8 +49,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 targetContent.style.opacity = '1';
                 targetContent.style.transition = 'opacity 0.5s ease';
                 const targetLines = targetContent.querySelectorAll('.animate-line');
-                targetLines.forEach(line => line.classList.add('active-line')); 
-                activeProduct = targetContent; 
+                targetLines.forEach(line => line.classList.add('active-line'));
+                activeProduct = targetContent;
             }
 
             if (activeCard) {
@@ -61,7 +61,76 @@ document.addEventListener('DOMContentLoaded', function () {
             activeCard = card;
         });
     });
+
+    // Poppup toggle logic
+    const buyNowButtons = document.querySelectorAll('.section-explore__buy-now');
+    const popups = document.querySelectorAll('.buy-now-popup');
+
+    function openPopup(popup) {
+        popup.style.display = 'block';
+        popup.classList.add('active');
+    }
+
+    function closePopup(popup) {
+        popup.style.display = 'none';
+        popup.classList.remove('active');
+    }
+
+    buyNowButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const popup = popups[index]; 
+            if (popup) {
+                openPopup(popup);
+
+                // Expand description logic
+                const learnMoreButton = popup.querySelector('.buy-now-popup__expand-desc');
+                const descriptionEl = popup.querySelector('.buy-now-popup__description');
+                const descriptionInner = popup.querySelector('.buy-now-popup__description-inner');
+
+                if (learnMoreButton) {
+                    learnMoreButton.addEventListener('click', () => {
+                        const expanded = descriptionEl.classList.contains('expanded');
+
+                        if (expanded) {
+                            descriptionEl.classList.remove('expanded');
+                            descriptionInner.style.height = '100px';
+                            learnMoreButton.querySelector('span').textContent = 'Learn More';
+                            descriptionInner.classList.add('with-gradient');
+                        } else {
+                            descriptionEl.classList.add('expanded');
+                            descriptionInner.style.height = `${descriptionInner.scrollHeight}px`;
+                            learnMoreButton.querySelector('span').textContent = 'Show Less';
+                            descriptionInner.classList.remove('with-gradient');
+                        }
+                    });
+
+                }
+
+                // Upsell checkbox toggle logic
+                const upsellBoxes = popup.querySelectorAll('.buy-now-popup__upsell-box');
+                upsellBoxes.forEach((box) => {
+                    const checkbox = box.querySelector('input[type="checkbox"]');
+
+                    box.addEventListener('click', () => {
+                        checkbox.checked = !checkbox.checked;
+                        if (checkbox.checked) {
+                            box.classList.add('active');
+                        } else {
+                            box.classList.remove('active');
+                        }
+                    });
+                });
+            }
+        });
+    });
+
+    // Add click event listener to close icons
+    popups.forEach((popup) => {
+        const closeButtons = popup.querySelectorAll('.close-buy-now-popup');
+        closeButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                closePopup(popup); 
+            });
+        })
+    });
 });
-
-
-

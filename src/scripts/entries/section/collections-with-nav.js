611843ced4643131316bbfc299b81865e8dfe0a2
Dusan639 <dusan16stojanovic@gmail.com>
2024-10-13 23:26:01 +0200

    collections with nav section

---
+++ src/scripts/entries/section/collections-with-nav.js
@@ -0,0 +1,6 @@
+import { initComponent } from 'lib/components'
+import CollectionsWithNav from 'modules/collections-with-nav/section-collections-with-nav.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initComponent(CollectionsWithNav, 'CollectionsWithNav')
+})
